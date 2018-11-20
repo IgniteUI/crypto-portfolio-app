85e0af64dc4d4777c5f662a9248b3db0f0b8d8a1
HristoP96 <HPopov@infragistics.com>
2018-11-20 16:53:00 +0200

    Adding dropdown with all the crypto coins and search input.

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  Component, OnInit, ViewEncapsulation, ViewChild, OnDestroy, AfterViewInit, ChangeDetectionStrategy,
+  ChangeDetectorRef
+} from '@angular/core';
 import { DataService } from '../data.service';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs/Observable';
-import {IgxLabelDirective} from 'igniteui-angular';
+import {
+  IgxLabelDirective, IgxFilterOptions,
+  HorizontalAlignment,
+  VerticalAlignment,
+  ConnectedPositioningStrategy,
+  CloseScrollStrategy,
+  IgxDropDownComponent
+} from 'igniteui-angular';
+import { ItemService } from '../block-item.service';
+class CryptoCoin {
+  name: string;
+  symbol: string;
+}
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class StatisticsComponent implements OnInit {
+export class StatisticsComponent implements AfterViewInit {
+  public coins: CryptoCoin[];
   public cryptoName;
   public daysCount;
   data: any;
-
-  constructor(private dataService: DataService, private route: ActivatedRoute) {
+  @ViewChild('dropDown', { read: IgxDropDownComponent }) public dropDown: IgxDropDownComponent;
+  public int = 0;
+  constructor(private dataService: DataService, private route: ActivatedRoute, private cdr: ChangeDetectorRef) {
 
     this.route
       .paramMap
@@ -26,11 +44,37 @@ export class StatisticsComponent implements OnInit {
       .pipe(map(params => params.get('daysCount') || route.routeConfig.data.daysCount)).subscribe(res => this.daysCount = res);
   }
 
-  ngOnInit() {
-    this.getData();
+  private _dropdownPositionSettings = {
+    horizontalStartPoint: HorizontalAlignment.Left,
+    verticalStartPoint: VerticalAlignment.Bottom
+  };
+
+  private _dropDownOverlaySettings = {
+    closeOnOutsideClick: true,
+    modal: false,
+    positionStrategy: new ConnectedPositioningStrategy(this._dropdownPositionSettings),
+    scrollStrategy: new CloseScrollStrategy()
+  };
+
+  public toggleDropDown(eventArgs, selectedDropDown: IgxDropDownComponent) {
+    const dropDown = selectedDropDown;
+    this._dropDownOverlaySettings.positionStrategy.settings.target = eventArgs.target;
+    dropDown.toggle(this._dropDownOverlaySettings);
   }
 
-  getData() {
+  ngAfterViewInit() {
+    this.getData();
+    this.getAllData();
+  }
+
+  getData(event?: any) {
+    let coin: any;
+    if (event) {
+      const name = event.event.target.innerText.substring(0, event.event.target.innerText.search('[[]')).trim();
+      const symbol = event.event.target.innerText.substring(event.event.target.innerText.search('[[]'));
+      coin = this.coins.find(c => c.name === name || c.symbol === symbol.replace('[', '').replace(']', ''));
+      this.cryptoName = coin.symbol;
+    }
     this.dataService.getLastSevenDaysPrices(this.cryptoName, this.daysCount)
       .subscribe(res => {
         this.data = Object.assign(res).Data.map(item => {
@@ -40,6 +84,36 @@ export class StatisticsComponent implements OnInit {
 
           return item;
         });
+        this.cdr.detectChanges();
       });
+
+  }
+
+  public getAllData() {
+    this.dataService.getData().map((data: any[]) => {
+      const obj = [];
+      for (let index = 0; index < data.length; index++) {
+        const name = data[index]['name'];
+        const symbol = data[index]['symbol'];
+        obj.push({ name: name, symbol: symbol });
+      }
+      return obj;
+    }).subscribe(res => this.coins = res);
+  }
+
+  get filterNames() {
+    const fo = new IgxFilterOptions();
+    fo.items = this.coins;
+    fo.key = 'symbol';
+    fo.inputValue = this.cryptoName;
+    if (fo.items) {
+      const fI = fo.items.find(item => {
+        return item.name.toUpperCase().includes(fo.inputValue.toUpperCase());
+      });
+      if (fI) {
+        fo.key = 'name';
+      }
+    }
+    return fo;
   }
 }
