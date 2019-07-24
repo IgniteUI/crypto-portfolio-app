59938236097edd2ea7f8aa0626a6934ac1109c51
Zdravko Kolev <ZKolev@infragistics.com>
2019-07-24 10:15:34 +0300

    Updating gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require('gulp'); 
+const fs = require("fs");
 
 gulp.task('build-site', () => {
     gulp.src(['dist/**/*', '!dist/**/index*/',])
         .pipe(gulp.dest('docs'));
+});
+
+gulp.task("overwrite-package-json", (done) => {
+    const packagesPaths = ["./node_modules/igniteui-angular-charts/package.json", "./node_modules/igniteui-angular-core/package.json"];
+    packagesPaths.forEach((packagePath) => {
+        const package = require(packagePath);
+        fs.writeFileSync(packagePath, JSON.stringify(package));
+    });
+    done();
 });
