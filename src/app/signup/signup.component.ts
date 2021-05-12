e71212ab621d57417d6b0d121c2aa051e1bc193b
Zdravko Kolev <ZKolev@infragistics.com>
2021-05-12 18:41:13 +0300

    ng update migrations

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -29,7 +29,7 @@ export class SignupComponent implements OnInit {
                this.router.navigate(['/login']);
             }).catch(
                (err) => {
-                  this.snack.show();
+                  this.snack.open();
                   this.error = err;
             });
       }
