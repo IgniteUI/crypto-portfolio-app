316bb71101d6058730e05add07d7c2d72317738e
dobromirts <dobromir.ts@gmail.com>
2023-11-10 13:32:17 +0200

    fix authentication

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NgZone } from '@angular/core';
 import firebase from 'firebase/compat/app';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Auth, GoogleAuthProvider, signInWithPopup, signOut, user } from  '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
 
@@ -42,7 +43,7 @@ export class AuthServiceService {
   }
 
   googleAuth() {
-    return this.authLogin(new firebase.auth.GoogleAuthProvider());
+    return this.authLogin(new  GoogleAuthProvider());
   }
 
   authLogin(provider) {
